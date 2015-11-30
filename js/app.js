961b6b62930d67469a60b824f07b43d04251b345
Christa Favano <christa@greatprofessionalwebsites.com>
2015-11-30 15:54:13 -0800

    Created event listener to add more rows to store section

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//create storeButton var
+var storeButton = document.getElementById('store-more');
+
+//Add event listener: click 
+var showMore = storeButton.addEventListener('click', addPlants, false);
+
+function addPlants() {
+  document.getElementsByClassName('hidden-store')[0].setAttribute('class', 'show');
+}
