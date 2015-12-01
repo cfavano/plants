170eaa84d8b017d4d0aef8895de32f01fd3fdf7c
Christa Favano <christa@greatprofessionalwebsites.com>
2015-12-01 09:30:44 -0800

    Added JS to unhide rows in store

--- js/app.js
+++ js/app.js
@@ -1,9 +1,10 @@
 //create storeButton var
 var storeButton = document.getElementById('store-more');
 
-//Add event listener: click 
+//Add event listener: click view more items in store
 var showMore = storeButton.addEventListener('click', addPlants, false);
 
+// function: unhide store row
 function addPlants() {
   document.getElementsByClassName('hidden-store')[0].setAttribute('class', 'unhidden-store show');
 }
