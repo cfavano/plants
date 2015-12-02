767f53f96ec2f8531b503f87f38f9ba86ffc88ce
Christa Favano <christa@greatprofessionalwebsites.com>
2015-12-02 09:56:13 -0800

    Changed class names on hidden rows in store

--- js/app.js
+++ js/app.js
@@ -1,11 +1,16 @@
 //create storeButton var
 var storeButton = document.getElementById('store-more');
 
+
+
+var dood = document.getElementsByClassName('hidden-store div');
+console.log(dood);
+
 //Add event listener: click view more items in store
 var showMore = storeButton.addEventListener('click', addPlants, false);
 
 // function: unhide store row
 function addPlants() {
-  document.getElementsByClassName('hidden-store')[0].setAttribute('class', 'unhidden-store show');
+  document.getElementsByClassName('hidden-row')[0].setAttribute('class', 'unhidden-store show');
+  incrementClick();
 }
-
